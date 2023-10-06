5a7c026733a781d9bbfac1f9b49971104761e013
WhidanB <danyphone95@gmail.com>
2023-10-06 09:32:55 +0200

    no more double init, added click handle and image choice

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,96 @@
 import {
   FaceDetector,
-  FilesetResolver,
-  Detection
+  FilesetResolver
 } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
-import { useEffect } from "react";
-import zidane from "./assets/zidane.jpg"
-
-let faceDetector = FaceDetector;
-let runningMode = "IMAGE";
-
+import React, { useEffect, useState } from "react";
+import zidane from "./assets/zidane.jpg";
+import kaamelott from "./assets/kaamelott.jpg";
+import starwars from "./assets/starwars.jpg";
 
 function App() {
-    const initializefaceDetector = async () => {
-      const vision = await FilesetResolver.forVisionTasks(
+    const [nameImages, setNameImages] = useState(zidane);
+    const initializeFaceDetector = async () => {
+    const vision = await FilesetResolver.forVisionTasks(
         "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
-        );
-        const faceDetector = await FaceDetector.createFromOptions(vision, {
-          baseOptions: {
-            modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite`,
-            delegate: "GPU"
-          },
-          runningMode: runningMode
-        });
-
-        const image = document.getElementById("image");
-        const detections = faceDetector.detect(image).detections[0];
-        const box = detections.boundingBox;
-        const keypoints = detections.keypoints;
-
-        let BoundingBox = document.createElement("div");
-        BoundingBox.classList="highlighter";
-        BoundingBox.style.left= box.originX+"px";
-        BoundingBox.style.top= box.originY+"px";
-        BoundingBox.style.width= box.width+"px";
-        BoundingBox.style.height= box.height+"px";
-        document.body.appendChild(BoundingBox);
-
-        for(let i=0; i<keypoints.length; i++){
-          let keypoint = document.createElement("div");
-          keypoint.className = "key-point";
-          keypoint.style.left = keypoints[i].x * box.width  - 3 + "px";
-          keypoint.style.top = keypoints[i].y * box.height - 3 + "px";
-          keypoint.style.width = "3px"
-          BoundingBox.appendChild(keypoint);
-          
+      );
+    const faceDetector = await FaceDetector.createFromOptions(vision, {
+        baseOptions: {
+          modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite`,
+          delegate: "GPU",
+        },
+        runningMode: "IMAGE",
+      });
+      const images = document.querySelectorAll(".image");
+      for (const image of images) {
+        const faceDetectorResult = await faceDetector.detect(image);
+        const detections = faceDetectorResult.detections;
         }
-        console.log(keypoints);
-        console.log(box);
-        console.log(detections);
-        // console.log(detections);
-        // console.log(keypoints);
+      images.forEach(async (image, index) => {
+        const faceDetectorResult = await faceDetector.detect(image);
+        const detections = faceDetectorResult.detections;
+        // test terminé, ligne suivante test supplémentaire
+        if (detections.length > 0) {
+          detections.forEach((detection) => {
+            const box = detection.boundingBox;
+            const keypoints = detection.keypoints;
+            const imageContainer = image.parentNode;
 
-      }
+            const boundingBox = document.createElement("div");
+            boundingBox.classList = "highlighter";
+            boundingBox.style.left = box.originX + "px";
+            boundingBox.style.top = (box.originY+image.offsetTop) + "px";
+            boundingBox.style.width = box.width + "px";
+            boundingBox.style.height = box.height + "px";
+            imageContainer.insertBefore(boundingBox, image);
 
+            keypoints.forEach((keypoint) => {
+              const keypointElement = document.createElement("div");
+              keypointElement.className = "key-point";
+              keypointElement.style.left =
+                (keypoint.x * image.width) + image.offsetLeft - 3 + "px";
+              keypointElement.style.top =
+                (keypoint.y * image.height) + image.offsetTop - 3 + "px";
+              keypointElement.style.width = "3px";
+              imageContainer.appendChild(keypointElement);
+            });
+          });
+        }
+      });
+    };
 
-        initializefaceDetector();
+        const handleNameImagesChange = (event) => {
+          setNameImages(event.target.value);
+          const boundingBoxes = document.querySelectorAll(".highlighter");
+          boundingBoxes.forEach((boundingBox)=>{
+            boundingBox.remove();
+          })
+          const keypoints = document.querySelectorAll(".key-point")
+          keypoints.forEach((keypoint)=>{
+            keypoint.remove();
+          })
+        };
 
-
-      
-
+        const handleclick= ()=>{
+          initializeFaceDetector();
+        }
 
   return (
     <div className="App">
+      <div>
+        <label>Image :</label>
+        <select onChange={handleNameImagesChange} value={nameImages}>
+          <option value={zidane}>Zidane</option>
+          <option value={kaamelott}>Kaamelott</option>
+          <option value={starwars}>Star Wars</option>
+        </select>
+      </div>
       
-        <img id='image' src={zidane} />
+        <div className="imageContainer" onClick={handleclick}>
+          <img className="image" src={nameImages} />
+        </div>
       
     </div>
   );
-
 }
 
 export default App;
