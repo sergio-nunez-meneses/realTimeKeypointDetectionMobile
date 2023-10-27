4fc6835c3d46d231ce6204d60066d936c9eab995
sergionunezmeneses <snunezmeneses@gmail.com>
2023-10-27 16:49:39 +0200

    Change code style (vision.js)

--- src/models/vision.js
+++ src/models/vision.js
@@ -1,8 +1,7 @@
 import {FilesetResolver} from "https://cdn.skypack.dev/@mediapipe/tasks-vision@latest";
 
-
 const vision = await FilesetResolver.forVisionTasks(
-    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
-  );
+		"https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm",
+);
 
-  export default vision;
+export default vision;
