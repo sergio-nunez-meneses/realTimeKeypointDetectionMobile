43dd213687df97e13962af07b3e785dcdab44b3c
WhidanB <danyphone95@gmail.com>
2023-11-08 16:42:15 +0100

    big styling improvements (App.css). Changed "isDetecting" possible value to boolean from 0 and 1. (Apps.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,21 @@ import models from "./models/Models";
 const OSC = require("osc-js")
 
 let selectedModel;
-let video, canvas, ctx, animation;
+let video, canvas, modal, ctx, animation;
 let dataToSend = [];
 let message;
 
 function App() {
-	const [isDetecting, setIsDetecting] = useState(0);
+	const [isDetecting, setIsDetecting] = useState(false);
 	const [modelName, setModelName]     = useState("face");
+	const [showModal, setShowModal]     = useState(false);
 	const osc                           = new OSC({plugin: new OSC.WebsocketClientPlugin()});
 	osc.open();
 
 	useEffect(() => {
 		video = document.getElementById("video");
 		video.addEventListener("loadeddata", () => {
+			modal             = document.getElementById("modal");
 			canvas            = document.getElementById("render");
 			canvas.width      = video.videoWidth;
 			canvas.height     = video.videoHeight;
@@ -28,14 +30,17 @@ function App() {
 			ctx         = canvas.getContext("2d");
 			models.draw = new DrawingUtils(ctx);
 		});
-	}, []);
+	}, [video]);
 
 	const start = () => {
+		modal.classList.remove("flex");
+		modal.classList.add("hidden");
+		setShowModal(false);
 		selectedModel = models[modelName];
 
 		runDetection();
 
-		setIsDetecting(1);
+		setIsDetecting(true);
 
 		canvas.classList.remove("hidden");
 	};
@@ -44,21 +49,21 @@ function App() {
 		setData();
 
 		// TODO: Process data
-		if (modelName === "hand") {
-			processHandData();
-		}
-		else if (modelName === "pose") {
-			processPoseData();
-		}
-		else if (modelName === "face") {
-			processFaceData()
-		}
-		/* TODO: Send data through OSC
-		Example:
-		const message = new OSC.Message("/model/landmark/coordinates", value);
-		osc.send(message);
-		*/
-		sendMessage();
+		// if (modelName === "hand") {
+		// 	processHandData();
+		// }
+		// else if (modelName === "pose") {
+		// 	processPoseData();
+		// }
+		// else if (modelName === "face") {
+		// 	processFaceData()
+		// }
+		// /* TODO: Send data through OSC
+		// Example:
+		// const message = new OSC.Message("/model/landmark/coordinates", value);
+		// osc.send(message);
+		// */
+		// sendMessage();
 
 
 		displayData();
@@ -178,14 +183,43 @@ function App() {
 		canvas.classList.add("hidden");
 
 		cancelAnimationFrame(animation);
-		setIsDetecting(0);
+		setIsDetecting(false);
 	};
+
+	const modalShow = () => {
+		if (showModal === false) {
+			setShowModal(true)
+			modal.classList.remove("hidden");
+			modal.classList.add("flex")
+		}
+		else {
+			setShowModal(false);
+			modal.classList.remove("flex")
+			modal.classList.add("hidden");
+		}
+
+
+	}
+
+	const setBackground = () => {
+		if (isDetecting === false) {
+			return ("red");
+		}
+		else {
+			return ("#23E95A");
+		}
+	};
+
 
 	return (
 			<div className="App">
 
-				<div className="modal">
-					<div className="model">
+				<div id="burger-menu" onClick={modalShow}>
+					<span className={`burger ${showModal ? 'cross' : 'line'}`}></span>
+				</div>
+
+				<div id="modal" className="hidden">
+					<div className="model_container">
 						<label>Model :</label>
 						<select onChange={(e) => {
 							setModelName(e.target.value);
@@ -195,19 +229,42 @@ function App() {
 							<option value={"hand"}>Hand</option>
 						</select>
 					</div>
+					<div className="warning">
+						<p>Press the button below to start detection. Press it again to stop the detection.</p>
+						<svg width="70" height="28" viewBox="0 0 70 28" fill="none"
+						     xmlns="http://www.w3.org/2000/svg">
+							<line y1="-3" x2="41.5404" y2="-3"
+							      transform="matrix(0.829259 0.558864 -0.829259 0.558864 0 4)" stroke="black"
+							      stroke-width="6"/>
+							<line y1="-3" x2="41.5404" y2="-3"
+							      transform="matrix(0.829259 -0.558864 0.829259 0.558864 35.5522 28)"
+							      stroke="black" stroke-width="6"/>
+						</svg>
+					</div>
 				</div>
 
 
 				<div className="webcam_container">
-					<Webcam id="video"/>
+					<Webcam id="video" audio={false} />
 					<canvas id="render" className="hidden canvas"/>
 				</div>
 
 				{/* TODO: Add containers based on the element's function */}
 				<div className="button">
-					<button onClick={!isDetecting ? start : stop}>
-						{!isDetecting ? "Start" : "Stop"} detection
+					<button onClick={!isDetecting ? start : stop} style={{backgroundColor: setBackground()}}>
+
 					</button>
+				</div>
+
+				<div className="infos">
+					<div className="IP">
+						<p>IP :</p>
+						<p>86.212.113.159</p>
+					</div>
+					<div className="UDP">
+						<p>UDP Port :</p>
+						<p>8000</p>
+					</div>
 				</div>
 			</div>
 	);
