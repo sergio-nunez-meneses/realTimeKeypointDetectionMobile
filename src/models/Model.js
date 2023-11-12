8abda46a909851b6c18a7ee4328eb42012cab256
sergionunezmeneses <snunezmeneses@gmail.com>
2023-11-12 22:15:12 +0100

    WIP add class Model (Model.js)

---
+++ src/models/Model.js
@@ -0,0 +1,10 @@
+import {DrawingUtils} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest";
+import {faceLandmarks, faceModel} from "./faceLandmarker";
+import {poseLandmarks, poseModel} from "./poseLandmarker";
+import {handLandmarks, handModel} from "./handLandmarker";
+
+
+export default class Model {
+	constructor() {
+	}
+}
