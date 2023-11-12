5cd8041ebba309e84bc2bec1b55955d67b6147d1
sergionunezmeneses <snunezmeneses@gmail.com>
2023-11-12 23:18:19 +0100

    Add class constructor (Model.js)

--- src/models/Model.js
+++ src/models/Model.js
@@ -5,6 +5,13 @@ import {handLandmarks, handModel} from "./handLandmarker";
 
 
 export default class Model {
-	constructor() {
+	constructor(video, context) {
+		this.video     = video;
+		this.context   = context;
+		this.draw      = new DrawingUtils(this.context);
+		this.model     = null;
+		this.modelName = null;
+		this.isFace    = null;
+		this.modelKey  = null;
 	}
 }
