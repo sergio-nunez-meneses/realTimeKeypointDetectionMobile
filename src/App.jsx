c87cbb30961f1aacce1060325e4c3a959b265bdf
WhidanB <danyphone95@gmail.com>
2023-11-15 10:37:37 +0100

    added getIpClient() function (App.jsx) to get ip address using Axios and Ipify

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import Webcam from "react-webcam";
 import Model from "./model/Model";
 import Osc from "./osc/Osc";
+import axios from 'axios';
 
 
 let video, canvas, modal, ctx, animation;
@@ -12,14 +13,17 @@ function App() {
 	const [isDetecting, setIsDetecting] = useState(false);
 	const [modelName, setModelName]     = useState("face");
 	const [showModal, setShowModal]     = useState(false);
+	const [userPort, setUserPort]       = useState(8000);
+	const [ipAddress, setIpAddress]     = useState()
 	const size                          = useWindowSize();
 
+	console.log(userPort)
 	// TODO: Replace with user data
-	const userPort = 8000;
-	const osc      = new Osc(userPort);
-	osc.open();
+	const osc = new Osc(userPort);
+	// osc.open();
 
 	useEffect(() => {
+		getIpClient();
 		video = document.getElementById("video");
 		video.addEventListener("loadeddata", () => {
 			modal             = document.getElementById("modal");
@@ -132,6 +136,15 @@ function App() {
 		return windowSize;
 	}
 
+	async function getIpClient() {
+		try {
+			const response = await axios.get('https://api.ipify.org?format=json');
+			setIpAddress(response.data.ip);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
 
 	return (
 			<div className="App">
@@ -153,7 +166,9 @@ function App() {
 					</div>
 					<div className="port_container">
 						<label>UDP Port:</label>
-						<input value="8000"/>
+						<input value={userPort} onChange={(e) => {
+							setUserPort(parseInt(e.target.value))
+						}}/>
 					</div>
 					<div className="landmarks_container">
 						<p>Landmarks</p>
@@ -173,7 +188,8 @@ function App() {
 						</div>
 					</div>
 					<div className="warning">
-						<p>Press the button below to start detection. Press it again to stop the detection.</p>
+						<p>Press the button below to start detection. Press it again to stop the
+							detection.</p>
 						<svg width="70" height="28" viewBox="0 0 70 28" fill="none"
 						     xmlns="http://www.w3.org/2000/svg">
 							<line y1="-3" x2="41.5404" y2="-3"
@@ -200,7 +216,8 @@ function App() {
 
 				{/* TODO: Add containers based on the element's function */}
 				<div className="button">
-					<button onClick={!isDetecting ? start : stop} style={{backgroundColor: setBackground()}}>
+					<button onClick={!isDetecting ? start : stop}
+					        style={{backgroundColor: setBackground()}}>
 
 					</button>
 				</div>
@@ -208,14 +225,16 @@ function App() {
 				<div className="infos">
 					<div className="IP">
 						<p>IP :</p>
-						<p>86.212.113.159</p>
+						<p>{ipAddress}</p>
 					</div>
 					<div className="UDP">
 						<p>UDP Port :</p>
-						<p>8000</p>
+						<p>{userPort}</p>
 					</div>
 				</div>
 			</div>
+
+
 	);
 }
 
