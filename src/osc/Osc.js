dc46f9cff37b17de35009d7258a813c487adbea8
sergionunezmeneses <snunezmeneses@gmail.com>
2023-11-20 17:02:50 +0100

    Remove property serverPort & Add method stop (Osc.js)

--- src/osc/Osc.js
+++ src/osc/Osc.js
@@ -2,13 +2,14 @@ import OSC from "osc-js";
 
 
 export default class Osc {
-	constructor(port) {
-		this.serverPort = port;
-		this.osc        = new OSC({plugin: new OSC.WebsocketClientPlugin()});
+	constructor() {
+		this.osc = new OSC({plugin: new OSC.WebsocketClientPlugin()});
 		this.osc.open();
 	}
 
-	// TODO: Add methods start and stop
+	stop() {
+		this.osc.close();
+	}
 
 	sendData(address, value) {
 		const message = new OSC.Message(address, value);
